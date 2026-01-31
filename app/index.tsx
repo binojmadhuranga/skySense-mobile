9c61640d7c07c77a28fcb6cd8953450e7cc1b1f5
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-01-31 15:22:09 +0530

    feat: implement layout and update index component with React Native structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
 function index() {
   return (
-    <div>index</div>
+    <View style={styles.container}>
+      <Text>index</Text>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default index
